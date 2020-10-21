842ee99cda87eed84901b2ee66de0411c4bad965
Maya <melokitty.1003@gmail.com>
2020-10-21 14:11:06 +0900

    画面遷移テスト

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import AppNavigator from "./src/navigation/AppNavigator";
+import SearchScreen from "./src/screens/SearchScreen";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <SearchScreen />
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={SearchScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+    // <View style={styles.container}>
+    //   <Text>Hello</Text>
+    // </View>
   );
 }
 
---
+++ src/navigation/AppNavigator.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+import SearchScreen from "../screens/SearchScreen";
+import TestScreen from "../screens/TextScreen";
+
+const Stack = createStackNavigator();
+
+const HomeStack = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="search" component={SearchScreen} />
+      <Stack.Screen name="test" component={TestScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default AppNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeStack} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
